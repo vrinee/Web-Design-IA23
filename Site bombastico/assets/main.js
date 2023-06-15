9c303ac7f79af618b32fdbb82d542d03b626498b
Brian Moura <brianmoura160e@gmaile.com>
2023-06-15 15:09:21 -0300

    deleted old site started new(menu)

---
+++ Site bombastico/assets/main.js
@@ -0,0 +1,10 @@
+var prevScrollpos = window.scrollY;
+window.onscroll = function() {
+  var currentScrollPos = window.scrollY;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("menu").style.top = "0";
+  } else {
+    document.getElementById("menu").style.top = "-15vh";
+  }
+  prevScrollpos = currentScrollPos;
+}
--- site principal/main.js
+++
@@ -1,8 +0,0 @@
-const curser = document.querySelector(".curser");
-document.addEventListener('mousemove', function(e){
-    let X = e.clientX;
-    let Y = e.clientY;
-
-    curser.style.left = X + "px";
-    curser.style.top = Y + "px";
-})
