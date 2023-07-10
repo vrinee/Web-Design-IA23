29e2609020db6795154ae68b4716075cc1afe410
Brian Moura <brianmoura160e@gmaile.com>
2023-07-10 10:48:44 -0300

    aumentei o código do jogo da velha

--- desafios e etc/jogo da velha/versão do zero/main.js
+++ desafios e etc/jogo da velha/versão do zero/main.js
@@ -1,18 +1,80 @@
 let tiles = document.querySelectorAll("div.jogo > *")
 let grid  = document.querySelector("div.jogo")
 
-grid.addEventListener("click",(ev) => {
+let player = 'X'
+let gameState = true
 
-    const tileIndex = [...tiles].indexOf(ev.target)
-    let aux = 1
+/*
 
-    if (aux == 1){
-        tiles[tileIndex].innerHTML = "X" 
-        console.log(aux) 
-        aux ++
+posições do tabuleiro
 
-    }else{
-        tiles[tileIndex].innerHTML = "O"
-        aux --
+[0][1][2]
+[3][4][5]
+[6][7][8]
+
+*/ 
+
+
+
+
+
+
+    
+
+
+function checkvalidation (tile) {
+
+    if(tile.innerText == 'X' || tile.innerText == 'O')
+    return false
+
+    return true
+
+}
+
+const winningConditions =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+function checkwincondition () {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        const a = board[winCondition[0]];
+        const b = board[winCondition[1]];
+        const c = board[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+}
+
+grid.addEventListener("click", (ev) => {
+
+    if (gameState){
+        const tileIndex = [...tiles].indexOf(ev.target)
+        if(tileIndex == -1)
+        return
+    
+        let valido =  checkvalidation (tiles[tileIndex])
+        if(valido)
+        {
+            tiles[tileIndex].innerText = player
+            player = player == 'X' ? 'O' : 'X' 
+        }
+        checkwincondition ()
+        if(roundWon)
+        gameState = false
     }
 })
