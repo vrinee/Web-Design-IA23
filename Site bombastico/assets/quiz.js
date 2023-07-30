554ee7ecfa931f8d6b84a7dc5a24e22d8f4cf271
Brian Moura <brianmoura160e@gmaile.com>
2023-07-30 17:30:29 -0300

    completei o quiz

--- Site bombastico/assets/quiz.js
+++ Site bombastico/assets/quiz.js
@@ -1,10 +1,10 @@
 const pergunta = document.querySelector(".pergunta")
 const alternativas = document.querySelector(".alternativas")
-
+const caixa = document.querySelector("#quiz > div")
 
 async function main() {
     const requisicao = await fetch("assets/quiz.json")
-    const quiz = await requisicao.json
+    const quiz = await requisicao.json()
     
     
     let questaoAtual = 0
@@ -12,9 +12,8 @@ async function main() {
     
     function carregarAlternativa(numeroDaAlternativa) {
         const alt = quiz[numeroDaAlternativa]
-        pergunta.innerHTML = alt.pergunta
+        pergunta.innerHTML = (numeroDaAlternativa + 1) + '/' + quiz.length + '&emsp;' + alt.pergunta
         alternativaCorretaAtual = alt.correta
-    
         alternativas.innerHTML = ""
         for (let i = 0; i <= 9 ;i++){
             alternativas.innerHTML += `<button>${alt.alternativas[i]}</button>`
@@ -27,10 +26,19 @@ async function main() {
     alternativas.addEventListener("click", ev => {
         const arrayChildren = [...alternativas.children]
         const alternativaCorretaClicada = arrayChildren.indexOf(ev.target)
-        if (alternativaCorretaClicada == alternativaCorretaAtual)
+        if (alternativaCorretaClicada == alternativaCorretaAtual){
+            if(questaoAtual == quiz.length){
+                caixa.innerHTML = '<h2>Parabéns você completou o quiz!!!</h2>'
+                return
+            }
+    
             return carregarAlternativa(++questaoAtual)
+        }
+
         alert("errou seu miseravel filha d aputa nao merecedor da vida ,inutil")
     })
     
     carregarAlternativa(questaoAtual)
 }
+
+main ()
