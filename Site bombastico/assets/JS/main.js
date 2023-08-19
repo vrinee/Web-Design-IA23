7d5a15964c8cc0cc071588f7fb8c668be71e27c9
vrinee <81476128+vrinee@users.noreply.github.com>
2023-08-19 14:46:38 +0000

    modal

--- Site bombastico/assets/JS/main.js
+++ Site bombastico/assets/JS/main.js
@@ -13,3 +13,25 @@ window.onscroll = function() {
 }
 
 /*-----------------------------------*/
+
+/*modal/Easter Egg*/
+
+let modal = document.querySelector("div.modal")
+let close = modal.querySelector("button.close")
+let open = document.getElementById("easteregg")
+
+  
+open.addEventListener('click', () => {
+  modal.classList.add("open")
+})
+
+close.addEventListener('click', () => {
+  modal.classList.remove("open")
+})
+
+modal.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains("modal")) {
+    modal.classList.remove("open")
+  }
+})
+
